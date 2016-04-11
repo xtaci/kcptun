1e2367a1add9af7ddff53fd566fa2d049c9a475e
xtaci <daniel820313@gmail.com>
2016-04-11 17:58:29 +0800

    seperate CPU

--- build-release.sh
+++ build-release.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 gox github.com/xtaci/kcptun/client github.com/xtaci/kcptun/server
-tar -zcf kcptun-linux.tar.gz client_linux_* server_linux_*
-tar -zcf kcptun-darwin.tar.gz client_darwin_* server_darwin_*
-tar -zcf kcptun-windows.tar.gz client_windows_* server_windows_*
+tar -zcf kcptun-linux-x86.tar.gz client_linux_386 server_linux_386
+tar -zcf kcptun-darwin-x86.tar.gz client_darwin_386 server_darwin_386
+tar -zcf kcptun-windows-x86.tar.gz client_windows_386.exe server_windows_386.exe
+
+tar -zcf kcptun-linux-amd64.tar.gz client_linux_amd64 server_linux_amd64
+tar -zcf kcptun-darwin-amd64.tar.gz client_darwin_amd64 server_darwin_amd64
+tar -zcf kcptun-windows-amd64.tar.gz client_windows_amd64.exe server_windows_amd64.exe
+
+tar -zcf kcptun-linux-arm.tar.gz client_linux_arm server_linux_arm
