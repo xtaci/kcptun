298b4a07955a97153654f987b293534455ee45d5
xtaci <daniel820313@gmail.com>
2017-03-02 11:04:01 +0800

    adjusta default parameters

--- server/main.go
+++ server/main.go
@@ -282,13 +282,13 @@ func main() {
 
 		switch config.Mode {
 		case "normal":
-			config.NoDelay, config.Interval, config.Resend, config.NoCongestion = 0, 60, 2, 1
-		case "fast":
 			config.NoDelay, config.Interval, config.Resend, config.NoCongestion = 0, 50, 2, 1
+		case "fast":
+			config.NoDelay, config.Interval, config.Resend, config.NoCongestion = 0, 40, 2, 1
 		case "fast2":
-			config.NoDelay, config.Interval, config.Resend, config.NoCongestion = 1, 40, 2, 1
-		case "fast3":
 			config.NoDelay, config.Interval, config.Resend, config.NoCongestion = 1, 30, 2, 1
+		case "fast3":
+			config.NoDelay, config.Interval, config.Resend, config.NoCongestion = 1, 20, 2, 1
 		}
 
 		log.Println("version:", VERSION)
