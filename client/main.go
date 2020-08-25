79480dfd9a8017b61d6b325072d177c1f5e38a32
Max Lv <mlv@nvidia.com>
2020-08-25 10:43:16 +0800

    Update the VPN flag

--- client/main.go
+++ client/main.go
@@ -107,145 +107,145 @@ func main() {
 	myApp.Usage = "client(with SMUX)"
 	myApp.Version = VERSION
 	myApp.Flags = []cli.Flag{
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "localaddr,l",
 			Value: ":12948",
 			Usage: "local listen address",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "remoteaddr, r",
 			Value: "vps:29900",
 			Usage: "kcp server address",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "key",
 			Value:  "it's a secrect",
 			Usage:  "pre-shared secret between client and server",
-			EnvVar: "KCPTUN_KEY",
+			EnvVars: []string{"KCPTUN_KEY"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "crypt",
 			Value: "aes",
 			Usage: "aes, aes-128, aes-192, salsa20, blowfish, twofish, cast5, 3des, tea, xtea, xor, sm4, none",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "mode",
 			Value: "fast",
 			Usage: "profiles: fast3, fast2, fast, normal, manual",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "conn",
 			Value: 1,
 			Usage: "set num of UDP connections to server",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "autoexpire",
 			Value: 10,
 			Usage: "set auto expiration time(in seconds) for a single UDP connection, 0 to disable",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "scavengettl",
 			Value: 600,
 			Usage: "set how long an expired connection can live(in sec), -1 to disable",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "mtu",
 			Value: 1350,
 			Usage: "set maximum transmission unit for UDP packets",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "sndwnd",
 			Value: 128,
 			Usage: "set send window size(num of packets)",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "rcvwnd",
 			Value: 512,
 			Usage: "set receive window size(num of packets)",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "datashard,ds",
 			Value: 10,
 			Usage: "set reed-solomon erasure coding - datashard",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "parityshard,ps",
 			Value: 3,
 			Usage: "set reed-solomon erasure coding - parityshard",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "dscp",
 			Value: 0,
 			Usage: "set DSCP(6bit)",
 		},
-		cli.BoolFlag{
+		&cli.BoolFlag{
 			Name:  "nocomp",
 			Usage: "disable compression",
 		},
-		cli.BoolFlag{
+		&cli.BoolFlag{
 			Name:   "acknodelay",
 			Usage:  "flush ack immediately when a packet is received",
 			Hidden: true,
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:   "nodelay",
 			Value:  0,
 			Hidden: true,
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:   "interval",
 			Value:  50,
 			Hidden: true,
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:   "resend",
 			Value:  0,
 			Hidden: true,
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:   "nc",
 			Value:  0,
 			Hidden: true,
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "sockbuf",
 			Value: 4194304, // socket buffer size in bytes
 			Usage: "per-socket buffer in bytes",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "keepalive",
 			Value: 10, // nat keepalive interval in seconds
 			Usage: "seconds between heartbeats",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "snmplog",
 			Value: "",
 			Usage: "collect snmp to file, aware of timeformat in golang, like: ./snmp-20060102.log",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "snmpperiod",
 			Value: 60,
 			Usage: "snmp collect period, in seconds",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "log",
 			Value: "",
 			Usage: "specify a log file to output, default goes to stderr",
 		},
-		cli.BoolFlag{
+		&cli.BoolFlag{
 			Name:  "quiet",
 			Usage: "to suppress the 'stream open/close' messages",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "c",
 			Value: "", // when the value is not empty, the config path must exists
 			Usage: "config from json file, which will override the command from shell",
 		},
-		cli.BoolFlag{
+		&cli.BoolFlag{
 			Name:  "fast-open",
 			Usage: "Dummy flag, doesn't really do anything",
 		},
-		cli.BoolFlag{
+		&cli.BoolFlag{
 			Name:  "V",
 			Usage: "Enable VPN mode for shadowsocks-android",
 		},
@@ -405,10 +405,8 @@ func main() {
 					config.Quiet = quiet
 				}
 			}
-			if c, b := opts.Get("V"); b {
-				if vpn, err := strconv.ParseBool(c); err == nil {
-					config.Vpn = vpn
-				}
+			if _, b := opts.Get("V"); b {
+				config.Vpn = true
 			}
 		}
 
