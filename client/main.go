454cef279b103a4d95af00db6b7d5d2d86817610
Hugo Wang <w@mitnk.com>
2020-12-20 13:29:38 +0800

    update option description

--- client/main.go
+++ client/main.go
@@ -129,7 +129,7 @@ func main() {
 		cli.IntFlag{
 			Name:  "scavengettl",
 			Value: 600,
-			Usage: "set how long an expired connection can live(in sec), -1 to disable",
+			Usage: "set how long an expired connection can live(in sec)",
 		},
 		cli.IntFlag{
 			Name:  "mtu",
