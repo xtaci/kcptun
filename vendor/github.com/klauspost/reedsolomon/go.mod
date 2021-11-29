fbbb4181fb59e6734ca43d8f8bdf541026c15a4e
Jacob Gadikian <jacobgadikian@gmail.com>
2021-11-29 18:49:30 +0700

    Prepare for tm-kcp

--- vendor/github.com/klauspost/reedsolomon/go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/klauspost/reedsolomon
-
-go 1.14
-
-require (
-	github.com/klauspost/cpuid v1.2.4
-)
--- vendor/github.com/mmcloughlin/avo/x86/gen.go
+++
@@ -1,4 +0,0 @@
-package x86
-
-//go:generate avogen -output zctors.go ctors
-//go:generate avogen -output zctors_test.go ctorstest
