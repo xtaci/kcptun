863833353cfb3ba9dbbe4a080982da2c2eb214d3
xtaci <imap@live.com>
2024-12-27 22:27:05 +0800

    fix qpp usage

--- std/qpp.go
+++ std/qpp.go
@@ -32,25 +32,25 @@ import (
 type QPPPort struct {
 	underlying io.ReadWriteCloser // io.Writer is not enough, we need to close the underlying writer as well
 
-	qpp   *qpp.QuantumPermutationPad
+	pad   *qpp.QuantumPermutationPad
 	wprng *qpp.Rand
 	rprng *qpp.Rand
 }
 
-func NewQPPPort(underlying io.ReadWriteCloser, qpp *qpp.QuantumPermutationPad, seed []byte) *QPPPort {
+func NewQPPPort(underlying io.ReadWriteCloser, pad *qpp.QuantumPermutationPad, seed []byte) *QPPPort {
 	wprng := qpp.CreatePRNG(seed)
 	rprng := qpp.CreatePRNG(seed)
-	return &QPPPort{underlying, qpp, wprng, rprng}
+	return &QPPPort{underlying, pad, wprng, rprng}
 }
 
-func (port *QPPPort) Read(p []byte) (n int, err error) {
-	n, err = port.underlying.Read(p)
-	port.qpp.DecryptWithPRNG(p[:n], port.rprng)
+func (r *QPPPort) Read(p []byte) (n int, err error) {
+	n, err = r.underlying.Read(p)
+	r.pad.DecryptWithPRNG(p[:n], r.rprng)
 	return
 }
 
 func (r *QPPPort) Write(p []byte) (n int, err error) {
-	r.qpp.EncryptWithPRNG(p, r.wprng)
+	r.pad.EncryptWithPRNG(p, r.wprng)
 	return r.underlying.Write(p)
 }
 
